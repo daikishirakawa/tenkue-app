a75acc0f2f647f3a72a052f6ad6e62470a51331f
daiki shirakawa <redred.hdsd95@gmail.com>
2021-01-09 22:00:02 +0900

    バグ発生中: undefined Swiper

--- src/asset/js/slider.js
+++ src/asset/js/slider.js
@@ -1,1 +1,28 @@
 // 画像スライダーの実装はこちらに記述してください
+// `use strict`
+
+// {
+    (function () {
+        var elem = document.querySelector(".swiper-container");
+        if (elem !== null) { //swiper-containerがあれば…
+        var mySwiper = new Swiper(".swiper-container", {
+          loop: true, //ループさせる
+          effect: "fade", //フェードのエフェクト
+          autoplay: {
+            delay: 4000, //４秒後に次の画像へ
+            disableOnInteraction: false //ユーザー操作後に自動再生を再開する
+          },
+          speed: 2000 //２秒かけながら次の画像へ移動
+          navigation: { //左右のページ送りを有効にする
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+          },
+          pagination: { //円形のページネーションを有効にする
+            el: ".swiper-pagination"
+          }
+        });
+      }
+      })();
+
+     
+// }
