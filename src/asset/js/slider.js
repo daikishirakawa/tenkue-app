29efb28c7757101d1b9a8e3e6c13109fe35b1d27
daiki shirakawa <redred.hdsd95@gmail.com>
2021-01-09 23:56:21 +0900

    #03実装完了しましたコードレビューお願いします

--- src/asset/js/slider.js
+++ src/asset/js/slider.js
@@ -13,13 +13,7 @@
             disableOnInteraction: false //ユーザー操作後に自動再生を再開する
           },
           speed: 2000 //２秒かけながら次の画像へ移動
-          navigation: { //左右のページ送りを有効にする
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-          },
-          pagination: { //円形のページネーションを有効にする
-            el: ".swiper-pagination"
-          }
+ 
         });
       }
       })();
